a7a76dfae09e2c477653e3695a27b86c05114f14
MDN <mark.denardo@gmail.com>
2026-02-19 11:40:44 -0500

    Refactor Hydra script for new visuals and text

--- 031526.js
+++ 031526.js
@@ -1,17 +1,61 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+											// Reforesters Laboratory
+											// SUN 03/15/26
+											// 2PM
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// o0
 await loadScript("https://cdn.jsdelivr.net/gh/geikha/hyper-hydra@latest/hydra-text.js")
-
 await loadScript("https://hyper-hydra.glitch.me/hydra-text.js")
-bpm = 10
 
-s0.initImage("image.jpg")
-
-hydraText.font = "serif"
+hydraText.font = "monospace"
 hydraText.lineWidth = "2%"
-hydraText.fontSize = "200%"
-str = " Reforester Labratory Listening Room MDN 031526 12:30 PM"
-src(s0)
-	.scale([1, 2, 3].fast(10), [1, 2, 3, 4].fast(25))
-	.pixelate([99,98,97],[100,99,98])
-	.layer(text(str).modulateScrollX(voronoi(0.1,1,1)).modulate(o0).repeat())
+hydraText.fontSize = "35%"
+hydraText.fillStyle = "purple"
+str = " MDN Sound Clinic "
+str1 = "Reforesters Laboratory"
+str2 = "SUN 03/15/26"
+str3 = "14:00"
+solid(0)
+	.layer(text(str))
+
 	.out(o0)
-render(o0)
+
+// o1
+s0.initVideo("https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNTZqMnU5aGRmYWhmNDcxZDlxaHZ3aXdwMjl3ZXo2ZWNqdXBqd3V6bCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/fvCoIEB8YtKghURZcA/giphy.mp4")
+s1.initImage("image.jpg")
+src(s0)
+	.add(s1, [0.04, 0.7].smooth())
+	.add(o0)
+	.out(o1)
+
+
+
+render(o1)
