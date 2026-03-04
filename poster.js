561bbba897316274a603def80ce1bf93edba71e6
MDN <mark.denardo@gmail.com>
2026-03-04 01:18:40 -0500

    Add initial poster.js with hydra code

---
+++ poster.js
@@ -0,0 +1,10 @@
+// code hydra [+/-] p5 or drag + drop .js hydra/p5 file(s) here
+bpm = 106.844;
+shape(17.418)
+	.scale(0.556)
+	.repeat([1.076, 1.202, 16.322], 11.045)
+	.modulateRotate(o0, 0.168)
+	.add(o0, 0.08)
+	.out(o0);
+
+// MDN @ Reforester(s) 03/15/26 https://ra.co/events/2377832
